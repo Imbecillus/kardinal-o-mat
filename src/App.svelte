25921621f1063e85ac9d338d8df555325d3d7eff
Tim Kneisel <t.kneisel@tu-bs.de>
2025-04-21 11:22:02 +0200

    Let there be light

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import Quiz from "./views/Quiz.svelte";
+    import Start from "./views/Start.svelte";
+
+  let activeView: 'start' | 'quiz' | 'results' = $state('start');
+</script>
+
+<main>
+  <h1>Kardinal-O-Mat</h1>
+
+  {#if activeView === 'start'}
+    <Start onStart={() => {activeView = 'quiz'}}></Start>
+  {:else if activeView === 'quiz'}
+    <Quiz></Quiz>
+  {/if}
+</main>
+
+<style>
+</style>
---
+++ src/views/Start.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  type Props = {
+    onStart: () => void;
+  }
+
+  const { onStart }: Props = $props();
+</script>
+
+<div>
+  <div class="start-button-container">
+    <button class="start" onclick={onStart}>
+      Start
+    </button>
+  </div>
+
+  <div>
+    <p>
+      <em>The College of Cardinals Report</em> hat die <a href="https://collegeofcardinalsreport.com/where-they-stand/?_papabili=1">
+        Positionen der aussichtsreichsten Kardinäle für die nächste Papstwahl</a>
+      zusammengetragen. Diese Webseite bereitet diese Positionen nach dem Vorbild des <a href="https://www.wahl-o-mat.de/">Wahl-O-Mat</a> auf.
+    </p>
+    <p>
+      Der Kardinal-O-Mat ist keine Wahlempfehlung. Die Chancen stehen ohnehin nicht sehr gut, dass Sie wahlberechtigt sind. :(
+    </p>
+  </div>
+</div>
+
+<style>
+  .start-button-container {
+    text-align: right;
+  }
+
+  .start {
+    background-color: var(--col-white);
+    border: none;
+    cursor: pointer;
+    font-size: 2rem;
+    min-width: 270px;
+
+    padding: 10px;
+
+    transition: all .2s;
+
+    &:hover {
+      background-color: var(--col-black);
+      color: var(--col-white);
+    }
+  }
+</style>
