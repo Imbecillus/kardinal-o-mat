70a4bc07d1163648235d727edbd664553d71c2d6
Tim Kneisel <t.kneisel@tu-bs.de>
2025-04-21 11:47:00 +0200

    add Question component

---
+++ src/lib/Question.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+  type Props = {
+    index: number;
+    total: number;
+
+    heading: string;
+    thesis: string;
+  }
+
+  const { index, total, heading, thesis }: Props = $props();
+</script>
+
+<div class="question">
+  <h3>
+    {index}/{total}
+    {heading}
+  </h3>
+  <p class="thesis">
+    {thesis}
+  </p>
+  <div class="answers">
+    <button>stimme zu</button>
+    <button>neutral</button>
+    <button>stimme nicht zu</button>
+  </div>
+  <button class="skip-thesis">These überspringen</button>
+</div>
+
+<style lang="css">
+  .question {
+    position: relative;
+
+    background-color: var(--col-white);
+
+    padding: 10px 55px 10px 55px;
+
+    text-align: left;
+
+    h3 {
+      margin: 0;
+    }
+  }
+
+  .thesis {
+    font-size: 2rem;
+  }
+
+  .answers {
+
+    button {
+      min-width: 180px;
+
+      background-color: var(--col-orange);
+
+      font-size: 1rem;
+
+      border: none;
+      padding: 5px;
+
+      cursor: pointer;
+
+      transition: all .2s;
+
+      &:hover {
+        background-color: var(--col-black);
+        color: var(--col-white);
+      }
+    }
+
+  }
+
+  .skip-thesis {
+    background-color: inherit;
+
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+
+    border: none;
+
+    cursor: pointer;
+    text-decoration: underline;
+
+    &:hover {
+      text-decoration: none;
+    }
+  }
+</style>
--- src/views/Quiz.svelte
+++ src/views/Quiz.svelte
@@ -1,3 +1,14 @@
+<script lang="ts">
+
+    import Question from "../lib/Question.svelte";
+
+</script>
+
 <div>
-  <h2>Quiz</h2>
+  <Question
+    heading="Ordination weiblicher Diakone"
+    thesis="Entgegen der von Papst Johannes Paul II. festgeschriebenen Tradition sollen auch Frauen als Diakoninnen ordiniert werden können."
+    index={1}
+    total={10}
+  ></Question>
 </div>
